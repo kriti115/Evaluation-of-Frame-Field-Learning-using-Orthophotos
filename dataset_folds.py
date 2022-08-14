c05a90e903b5327a9d636c6726533ff259f4269b
Kriti Shrestha <kritishrestha523@gmail.com>
2022-08-14 20:59:00 +0200

    Update dataset_folds.py

--- dataset_folds.py
+++ dataset_folds.py
@@ -116,6 +116,7 @@ def get_private_dataset_folds(config, root_dir, folds):
     return ds_list
 
 
+''' Add here for your own dataset '''
 
 
 def get_folds(config, root_dir, folds):
@@ -127,6 +128,8 @@ def get_folds(config, root_dir, folds):
     
     elif config["dataset_params"]["root_dirname"] == "PrivateDataset":
         return get_private_dataset_folds(config, root_dir, folds)
+    
+    ''' Add here for your own dataset '''
 
     else:
         print_utils.print_error("ERROR: config[\"data_root_partial_dirpath\"] = \"{}\" is an unknown dataset! "
